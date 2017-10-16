acd06d8c059d6e6f09c879516c98cec9529141f5
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-10-16 23:34:17 +0200

    Test of the new navbar styles.

---
+++ java-r.js
@@ -0,0 +1,23 @@
+function IconNavbar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
