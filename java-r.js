68a5d24eff020e7dd147ffb4cc09fe789f97963c
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-10-19 22:41:11 +0200

    I found out, why the styling did not work. I copy pasted the structure for every listing, so that I used the th (table-header)-tag instead of the td-tag. And I added the javascript for the search-function. Problem: It only searches in the first column, maybe I can change it, so that it searches for everything.

--- java-r.js
+++ java-r.js
@@ -61,3 +61,29 @@ for (i = 0; i < locations.length; i++) {
 }
 
 //END: map and locations
+
+
+//START: Filter project table
+
+function projectFilter() {
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("projects");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
+//END: Filter project table
