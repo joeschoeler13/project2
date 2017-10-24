cfe80072410033559457420ad60e47917b2ced5e
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-10-24 04:37:41 +0200

    I found and erased the bug for the NNSbug. It was a simple variable scope bug. The goal of this branch is therefore reached.

--- java-all.js
+++ java-all.js
@@ -1,3 +1,17 @@
+//START: Sticky navbar section
+
+$(window).scroll(function() {
+    var navbar = document.getElementById("navbar1");
+    if ($(this).scrollTop() > 370) {
+        navbar.className += " sticky";
+    } else {
+        navbar.className = "navbar";
+    }
+})
+
+//END: Sticky navbar section
+
+
 //START: Responsive navbar section
 
 function respNavbar() {
--- java-r.js
+++ java-r.js
@@ -1,18 +1,3 @@
-//START: Sticky navbar section
-
-var navbar = document.getElementById("navbar1");
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 370) {
-        navbar.className += " sticky";
-    } else {
-        navbar.className = "navbar";
-    }
-})
-
-//END: Sticky navbar section
-
-
 //START: map and locations
 
 var locations = [
