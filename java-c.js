9407a924285050017aa9da95df2b9dfafc15534c
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-10-24 04:58:19 +0200

    I found the reason why the button in the form did not work - I forgot the brackets in the function-call -onclick='submitform' as it was initially. It works now, therefore this branch can be closed.

--- java-c.js
+++ java-c.js
@@ -1,22 +1,7 @@
-//START: Sticky navbar section
-
-var navbar = $(".navbar");
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 370) {
-        navbar.addClass("sticky");
-    } else {
-        navbar.removeClass("sticky");
-    }
-})
-
-//END: Sticky navbar section
-
-
 //START: Mail-submitted
 
 function submitform() {
-    alert("Thank you very much! \n We received your message and will now start working on it. \n We will get back to you as soon as possible.")
+    alert("Thank you very much! \nWe received your message and will now start working on it. \nWe will get back to you as soon as possible.")
 }
 
 //END: Mail submitted
